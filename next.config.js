f14de3a0aab1632f2bff789b6b26448397702fea
1chiSensei <mail@1chi.tk>
2021-06-27 23:05:04 +0800

    fix: ignore eslint

--- next.config.js
+++ next.config.js
@@ -15,5 +15,8 @@ module.exports = withBundleAnalyzer(
             runtimeCaching,
         },
         webpack: (config) => config,
+        eslint: {
+            ignoreDuringBuilds: true,
+        },
     }),
 );
