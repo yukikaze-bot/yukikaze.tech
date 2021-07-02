d4cfab413f75071cef2600aee32f29acf92f9cd0
1chiSensei <mail@tomio.codes>
2021-07-02 22:13:32 +0800

    feat: add discord rpc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { emit } from '@tauri-apps/api/event';
+
+const onload = () => emit('loaded');
 
 class MyDocument extends Document {
     public static async getInitialProps(ctx: any) {
@@ -10,7 +13,7 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head />
-                <body className="dark-theme github">
+                <body className="dark-theme github" onLoad={onload}>
                     <Main />
                     <NextScript />
                 </body>
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,38 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use rustcord::{Rustcord, EventHandlers, User, RichPresenceBuilder};
+
+pub struct Handlers;
+
+impl EventHandlers for Handlers {
+    fn ready(user: User) {
+        println!("User {}#{} logged in...", user.username, user.discriminator);
+    }
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            let discord = Rustcord::init::<Handlers>("855428383574589460", true, None)?;
+            let presence = RichPresenceBuilder::new()
+                .state("Yukikaze")
+                .details("Browsing the desktop app")
+                .large_image_key("yukikaze")
+                .large_image_text("Yukikaze")
+                .build();
+
+            app.listen_global("loaded", |event| {
+                println!("got loaded {:?}", event.payload());
+
+                discord.update_presence(presence)?;
+                loop {
+                    discord.run_callbacks();
+                }
+            });
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
