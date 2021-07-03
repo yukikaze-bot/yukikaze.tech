abd99fbab44d02fb1a2fcf98d796c12d01b70cbf
1chiSensei <mail@tomio.codes>
2021-07-03 21:27:55 +0800

    revert: remove discord rpc

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,31 +3,8 @@
     windows_subsystem = "windows"
 )]
 
-use discord_rich_presence::{activity, new_client, DiscordIpc};
-
 fn main() {
     tauri::Builder::default()
-        .setup(|app| -> Result<(), Box<dyn std::error::Error>> {
-            let mut client = new_client("855428383574589460")?;
-            client.connect()?;
-
-            let activity = activity::Activity::new()
-                .state("Yukikaze")
-                .details("Viewing the Yukikaze desktop app")
-                .assets(
-                    activity::Assets::new()
-                        .large_image("yukikaze")
-                        .large_text("Yukikaze"),
-                )
-                .buttons(vec![activity::Button::new(
-                    "Download",
-                    "https://yukikaze.tech/download",
-                )]);
-
-            client.set_activity(activity)?;
-
-            Ok(())
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
